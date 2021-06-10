db062bbcf10cfd0a1685105c7c52b3566cfc2a10
myerkes <mattyerkes23@gmail.com>
2021-06-10 17:52:05 -0400

    Correct routes

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,16 @@
+from polls.models import Question
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index!")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("aosidfnasoidnfadf")
+    return HttpResponse("You're looking at the results for question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
