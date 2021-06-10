d69dba2ed9eed3e18366d01d17437980c416e153
myerkes <mattyerkes23@gmail.com>
2021-06-10 18:38:18 -0400

    Initial detail view or 404

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2#1+8h)msyktfka0e)_&jy3%u@+c%#zqggrs0(a6(3brkxi!%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
+from django.http.response import Http404
 from polls.models import Question
+from django.http import Http404
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -10,7 +12,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results for question %s." % question_id)
